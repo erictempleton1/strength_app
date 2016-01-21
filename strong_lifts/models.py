4ec0b0f12363664a7031a407e56e7623e6dbb985
Eric Templeton <erictempleton1@gmail.com>
2016-01-20 21:00:27 -0500

    adding models, and adding app to settings

--- strong_lifts/models.py
+++ strong_lifts/models.py
@@ -2,4 +2,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class StrongLifts(models.Model):
+    updated_date = models.DateTimeField()
+    exercise_type = models.CharField(max_length=200)
+    exercise_set = models.IntegerField(default=0)
+    exercise_rep = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.exercise_type
