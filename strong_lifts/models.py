7579cbde4a07110dda282c44996fa0bd66f5b71d
Eric Templeton <erictempleton1@gmail.com>
2016-01-23 16:19:35 -0500

    using datetime as default

--- strong_lifts/models.py
+++ strong_lifts/models.py
@@ -1,10 +1,12 @@
 from __future__ import unicode_literals
 
+import datetime
 from django.db import models
 from django.utils import timezone
 
+
 class StrongLifts(models.Model):
-    add_at = models.DateField()
+    add_at = models.DateField(default=datetime.datetime.now)
     exercise_name = models.CharField(max_length=200)
     exercise_set = models.IntegerField(default=0)
     exercise_rep = models.IntegerField(default=0)
