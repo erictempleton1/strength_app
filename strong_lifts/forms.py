bf31beda6221bc6386c600812b9ab3444a5de333
Eric Templeton <erictempleton1@gmail.com>
2016-01-23 10:45:30 -0500

    adding basic registration form

---
+++ strong_lifts/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from django.contrib.auth.models import User
+
+
+class RegisterForm(forms.ModelForm):
+    username = forms.CharField(required=True)
+    email = forms.EmailField(required=True)
+    password = forms.CharField(widget=forms.PasswordInput(), required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
--- strong_lifts/views.py
+++ strong_lifts/views.py
@@ -1,14 +1,38 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 from django.contrib.auth import authenticate, login, logout
+
+from forms import RegisterForm
 
 
 def index(request):
     return render(request, 'strong_lifts/index.html')
 
 def register(request):
-    return render(request, 'strong_lifts/register.html')
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            #form.save()
+            return HttpResponseRedirect('/stronglifts')
+    else:
+        form = RegisterForm()
+    return render(request, 'strong_lifts/register.html', {'form': form})
 
 def login_user(request):
+    username = request.post['username']
+    password = request.post['password']
+    user = authenticate(username=username, password=password)
+
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+        else:
+            pass
+            # return inactive user message here
+    else:
+        pass
+        # return invalid login message
     return render(request, 'strong_lifts/login.html')
 
 def logout_user(request):
